836926d93095d5afb5cbcb767bef7351f42c4382
Luiz Lencioni <luiz.lencioni@gmail.com>
2023-04-16 14:57:14 -0300

    Reducing variables

--- GetStockPrice.py
+++ GetStockPrice.py
@@ -21,28 +21,23 @@ Ticker = "ITSA4"
 # Get stock prices via Yahoo Finance
 ######################################
 
-pega = yf.Ticker(Ticker + ".SA").history("1d")
-
-# We want just the close price
-clousure_info = pega['Close']
-print("\n")
-print(clousure_info)
-print("\n")
+last_day_stock_info = yf.Ticker(Ticker + ".SA").history("1d")
 
 # Get Stock Price (get rid of index, square brackets, convert array to float)
-clousure_info = clousure_info.values
-stock_price = clousure_info.astype(str)
+stock_price = last_day_stock_info['Close']
+stock_price = stock_price.values
+stock_price = stock_price.astype(str)
 stock_price = ''.join(stock_price)
 stock_price = float(stock_price)
 stock_price = round(stock_price, 2)
 
 # Get Date (convert date to string and get rid of square brackets)
-data_cotacao = pega.index.date
-stock_date = data_cotacao.astype(str)
+stock_date = last_day_stock_info.index.date
+stock_date = stock_date.astype(str)
 stock_date = ''.join(stock_date)
 
 # Summary: clousure price and date
-print(Ticker)
+print("\n" + Ticker)
 print("Date  : " + stock_date)
 print("Price : " + str(stock_price))
 
