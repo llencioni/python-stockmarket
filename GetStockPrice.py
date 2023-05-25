7284cebbffeb7c8ddc6d0d6af1ac3de3567799b7
Luiz Lencioni <luiz.lencioni@gmail.com>
2023-05-25 17:42:51 -0300

    Adding a GUI done in QtDesiner

--- GetStockPrice.py
+++ GetStockPrice.py
@@ -15,54 +15,87 @@ import datetime as dt
 import yfinance as yf
 import openpyxl
 from openpyxl.styles import Alignment
-from tkinter import *
 
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
+from PyQt5 import QtCore
+from stockGUI import *
+
+
+######################################
+# GUI class from QtDesigner
+######################################
+
+class Tela (QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        
+        # Buttons click
+        self.ui.button1.clicked.connect(self.get_stock_price)
+        self.ui.buttonOpenTXT.clicked.connect(self.getTxtFile)
 
 ######################################
 # Get stock prices via Yahoo Finance
 ######################################
 
-def get_stock_price (FileName, Ticker):
-
-    # open an excel file
-    excel_file = openpyxl.Workbook()
-    sheet = excel_file.active
-
-    # get stock info
-    for i in Ticker:        
-
-        stock_ticker = i + ".SA"
-        last_day_stock_info = yf.Ticker(stock_ticker).history("1d")
+    def getTxtFile (self):
         
-        # Get Stock Price (get rid of index, square brackets, convert array to float)
-        stock_price = last_day_stock_info['Close']
-        stock_price = stock_price.values
-        stock_price = stock_price.astype(str)
-        stock_price = ''.join(stock_price)
-        stock_price = float(stock_price)
-        stock_price = round(stock_price, 2)
+        self.FileTxt = QFileDialog.getOpenFileName(self, "", "", "Text (*.txt)")
         
-        # Get Date (convert date to string and get rid of square brackets)
-        stock_date = last_day_stock_info.index.date
-        stock_date = stock_date.astype(str)
-        stock_date = ''.join(stock_date)
+        if self.FileTxt:
+            print("\nFile name:" + self.FileTxt[0])
+
+    @QtCore.pyqtSlot()
+    def get_stock_price (self):
     
-        # Summary: clousure price and date
-        print("\n" + i)
-        print("Date :", stock_date)
-        print("Price :", str(stock_price))
+        # Read each line (tickers) and get rid of "\n" in the end of each string
+        with open(self.FileTxt[0],'r') as f:
+            Ticker = f.read().splitlines()
+    
+        # Name of the Excel file output"
+        FileName = "ACAO-FIIs.xlsx"
+    
+        # open an excel file
+        excel_file = openpyxl.Workbook()
+        sheet = excel_file.active
+    
+        # get stock info
+        for i in Ticker:        
+    
+            stock_ticker = i + ".SA"
+            last_day_stock_info = yf.Ticker(stock_ticker).history("1d")
+            
+            # Get Stock Price (get rid of index, square brackets, convert array to float)
+            stock_price = last_day_stock_info['Close']
+            stock_price = stock_price.values
+            stock_price = stock_price.astype(str)
+            stock_price = ''.join(stock_price)
+            stock_price = float(stock_price)
+            stock_price = round(stock_price, 2)
+            
+            # Get Date (convert date to string and get rid of square brackets)
+            stock_date = last_day_stock_info.index.date
+            stock_date = stock_date.astype(str)
+            stock_date = ''.join(stock_date)
         
-        # add info in the excel spreedsheet
-        sheet.append([i, stock_price, stock_date])
-
-    # center align stock price cells
-#    for row in sheet[1:sheet.max_row]:
-
-    #        cell = row[1]
-#        cell.alignment = Alignment(horizontal='center')
-
-    # save excel file
-    excel_file.save(FileName)
+            # Summary: clousure price and date
+            print("\n" + i)
+            print("Date :", stock_date)
+            print("Price :", str(stock_price))
+            
+            # add info in the excel spreedsheet
+            sheet.append([i, stock_price, stock_date])
+    
+        # center align stock price cells
+    #    for row in sheet[1:sheet.max_row]:
+    
+        #        cell = row[1]
+    #        cell.alignment = Alignment(horizontal='center')
+    
+        # save excel file
+        excel_file.save(FileName)
     
 
 ######################################
@@ -70,16 +103,10 @@ def get_stock_price (FileName, Ticker):
 ######################################
 
 if __name__ == "__main__":
-
-    # Read each line (tickers) and get rid of "\n" in the end of each string
-    with open('stock-fii-to-read.txt','r') as f:
-        input_tickers = f.read().splitlines()
-
-    # Name of the Excel file output"
-    output_file_name = "ACAO-FIIs.xlsx"
-
-    # Get values for each ticker and save it in the Excel file
-    get_stock_price(output_file_name, input_tickers)
+    app = QApplication(sys.argv)
+    w = Tela()
+    w.show()
+    sys.exit(app.exec_())
 
 
 
--- README.md
+++ README.md
@@ -4,4 +4,9 @@ How to get stock market information using Python
 To install all the packages needed to run this code:
 >> pip install -r requirements.txt
 
-** Put the tickers you are interested in the "stock-fii-to-read.txt" file
+** How to Used It
+
+. Put the tickers you are interested in the "stock-fii-to-read.txt" file
+
+. To convert GUI from QtDesigner:
+>> pyuic5 -x ./QtDesigner-files/StockMarket.ui -o stockGUI.py
